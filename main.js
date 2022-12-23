64d158c6f4570ef0f606b64969f3e29f57c7a914
ltoro1208 <luis_toro89@hotmail.com>
2022-12-22 21:57:47 -0500

    agregado JS a menu de usuario

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//---SHOW: PREFERENCE MENU--------------------------------------------//
 var preference_button = document.querySelector('.preference-button');
 var preference_menu = document.querySelector('.preference-menu');
 
@@ -7,4 +8,19 @@ function showPreferenceMenu(){
 preference_menu.classList.toggle('inactive');
 }
 
+//---SHOW: USER MENU--------------------------------------------------//
+var user_option_menu = document.querySelector('.user-option-menu');
+var profile_menu_button = document.querySelector('.profile-menu-button');
+var header_menu_span = document.querySelector('.header-menu .profile-menu-button')
 
+profile_menu_button.addEventListener('click', showUserOptionMenu);
+
+function showUserOptionMenu(){
+    user_option_menu.classList.toggle('inactive');
+    header_menu_span.classList.toggle('shadow');
+}
+
+//---CLOSE: USER MENU-------------------------------------------------//
+var close_user_menu = document.querySelector('.close-user-menu');
+
+close_user_menu.addEventListener('click', showUserOptionMenu);
